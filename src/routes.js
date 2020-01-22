612605928af4068fd7f2bb577aac150736edaea0
a <a>
2020-01-22 12:54:13 -0300

    End Module 02

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,15 @@ import { Router } from 'express';
 import UserControler from './app/controllers/UserController';
 import SessionControler from './app/controllers/SessionController';
 
+import authMiddeware from './app/middlewares/auth';
+
 const routes = new Router();
 
 routes.post('/users', UserControler.store);
 routes.post('/sessions', SessionControler.store);
+
+routes.use(authMiddeware);
+
+routes.put('/users', UserControler.update);
 
 export default routes;
