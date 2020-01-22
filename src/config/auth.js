ca936936b06839cd80f2c2b69d54e846107ff498
a <a>
2020-01-22 11:34:38 -0300

    JWT Auth

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '1a47b07eb3caedb603ba4eb552497dc8',
+  expiresIn: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import UserControler from './app/controllers/UserController';
+import SessionControler from './app/controllers/SessionController';
 
 const routes = new Router();
 
 routes.post('/users', UserControler.store);
+routes.post('/sessions', SessionControler.store);
 
 export default routes;
