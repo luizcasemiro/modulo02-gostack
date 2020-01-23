68c0ffa5bc8b98b77c20d2940721efb988935397
a <a>
2020-01-23 16:46:35 -0300

    Upload and place avatar id on database

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,16 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserControler from './app/controllers/UserController';
 import SessionControler from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 
 import authMiddeware from './app/middlewares/auth';
 
 const routes = new Router();
+
+const upload = multer(multerConfig);
 
 routes.post('/users', UserControler.store);
 routes.post('/sessions', SessionControler.store);
@@ -13,5 +18,6 @@ routes.post('/sessions', SessionControler.store);
 routes.use(authMiddeware);
 
 routes.put('/users', UserControler.update);
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
